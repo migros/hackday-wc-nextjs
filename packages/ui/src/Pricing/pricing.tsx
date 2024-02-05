88ae60d9afb0eb82113f75fc2d3a904e657ff4c1
Tim Gruenewald <tim.gruenewald@mgb.ch>
2024-02-05 10:55:47 +0100

    refactor(pricing-component): remove unused params

--- packages/ui/src/Pricing/pricing.tsx
+++ packages/ui/src/Pricing/pricing.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-interface PricingProps {}
-
-export const Pricing = (props: PricingProps) => {
+export const Pricing = () => {
     return (
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
